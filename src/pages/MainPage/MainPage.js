6edd1baff57475af0160d97cde2eeb9cea30e8fd
Администратор <xameleon@gmail.com>
2022-05-03 16:41:17 +0300

    first commit

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -7,6 +7,44 @@ import steel from '../../img/prem_img/steel.png'
 import svarshik from '../../img/prem_img/svarshik.png'
 import bolt from '../../img/prem_img/bolt.png'
 import nakovlnia from '../../img/prem_img/nakovlnia.png'
+
+import f_1 from '../../img/raboty_img/1.jpg'
+import f_2 from '../../img/raboty_img/2.jpg'
+import f_3 from '../../img/raboty_img/3.jpg'
+import f_4 from '../../img/raboty_img/4.jpg'
+import f_5 from '../../img/raboty_img/5.jpg'
+import f_6 from '../../img/raboty_img/6.jpg'
+import f_7 from '../../img/raboty_img/7.jpg'
+import f_8 from '../../img/raboty_img/8.jpg'
+
+import f_9 from '../../img/raboty_img/9.jpg'
+import f_10 from '../../img/raboty_img/10.jpg'
+import f_11 from '../../img/raboty_img/11.jpg'
+import f_12 from '../../img/raboty_img/12.jpg'
+import f_13 from '../../img/raboty_img/13.jpg'
+import f_14 from '../../img/raboty_img/14.jpg'
+import f_15 from '../../img/raboty_img/15.jpg'
+import f_16 from '../../img/raboty_img/16.jpg'
+
+import f_17 from '../../img/raboty_img/17.jpg'
+import f_18 from '../../img/raboty_img/18.jpg'
+import f_19 from '../../img/raboty_img/19.jpg'
+import f_20 from '../../img/raboty_img/21.jpg'
+import f_21 from '../../img/raboty_img/22.jpg'
+import f_22 from '../../img/raboty_img/23.jpg'
+import f_23 from '../../img/raboty_img/24.jpg'
+import f_24 from '../../img/raboty_img/25.jpg'
+
+import f_25 from '../../img/raboty_img/25.jpg'
+import f_26 from '../../img/raboty_img/26.jpg'
+import f_27 from '../../img/raboty_img/27.jpg'
+import f_28 from '../../img/raboty_img/28.jpg'
+import f_29 from '../../img/raboty_img/29.jpg'
+import f_30 from '../../img/raboty_img/30.jpg'
+import f_31 from '../../img/raboty_img/31.jpg'
+import f_32 from '../../img/raboty_img/32.jpg'
+
+
 import '../PremPage/PremPage.css'
 import '../RabPage/RabPage.css'
 import "../ZayavkaPage/ZayavkaPage.css";
@@ -56,7 +94,7 @@ const MainPage = () => {
                             <div>
                                 <div className="main-block__text">
                                     <div className="main-block__text__img">
-                                        <img src={metka_img}/><p>Наб. реки Фонтанки 10-15</p>
+                                        <img src={metka_img}/><p>г.Чистополь, ул.Пушкина, 133</p>
                                     </div>
                                     <div className="main-block__text__img">
                                         <img src={tel_img}/><p>8 (922) 030-00-07 <br/> 8 (987) 418-67-41</p> <br/>
@@ -112,41 +150,117 @@ const MainPage = () => {
                 </div>
             </div>
 
-            {/*<div className="page">*/}
-            {/*    <div className="rabota__container _container">*/}
-            {/*        <div className="text__title">*/}
-            {/*            Рабочий процесс*/}
-            {/*        </div>*/}
-            {/*        <div>*/}
-            {/*            <div className="services__body">*/}
-            {/*                <div className="services__column">*/}
-            {/*                    <div className="services__item item-service">*/}
-            {/*                        /!*<div className="img__block"></div>*!/*/}
-            {/*                        <h3 className="item-service__title">ТУТ ТЕКСТ</h3>*/}
-            {/*                    </div>*/}
-            {/*                </div>*/}
-            {/*                <div className="services__column">*/}
-            {/*                    <div className="services__item item-service">*/}
-            {/*                        /!*<div className="img__block"><img src={provoda}/></div>*!/*/}
-            {/*                        <h3 className="item-service__title">ТУТ ТЕКСТ</h3>*/}
-            {/*                    </div>*/}
-            {/*                </div>*/}
-            {/*                <div className="services__column">*/}
-            {/*                    <div className="services__item item-service">*/}
-            {/*                        /!*<div className="img__block"><img src={provoda}/></div>*!/*/}
-            {/*                        <h3 className="item-service__title" id="zaiavka">ТУТ ТЕКСТ</h3>*/}
-            {/*                    </div>*/}
-            {/*                </div>*/}
-            {/*                <div className="services__column">*/}
-            {/*                    <div className="services__item item-service">*/}
-            {/*                        /!*<div className="img__block"><img src={provoda}/></div>*!/*/}
-            {/*                        <h3 className="item-service__title">ТУТ ТЕКСТ</h3>*/}
-            {/*                    </div>*/}
-            {/*                </div>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+            <div className="page2">
+                <div className="rabota__container _container">
+                    <div className="text__title">
+                        Наши клиенты
+                    </div>
+                    <div>
+                        <div className="services__body">
+                            <div className="services__column">
+
+                                <div className="services__item">
+                                    <img src={f_1} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_2} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_3} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_4} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_5} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_6} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_7} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_8} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_9} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_10} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_11} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_12} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_13} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_14} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_15} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_16} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_17} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_18} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_19} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_20} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_21} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_22} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_23} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_24} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_25} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_26} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_27} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_28} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_29} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_30} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_31} alt="" className="services__img"/>
+                                </div>
+                                <div className="services__item">
+                                    <img src={f_32} alt="" className="services__img"/>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <div className="page1">
                 <div className="zayavkapage__container _container">
